05d27cc48c87479921fa027c8abdfa2c1c974ded
Alex Misuno <alex@misuno.org>
2017-05-22 16:07:04 +0200

    main menu controller logic

--- Assets/TestTaskMemory/Scripts/MainMenuController.cs
+++ Assets/TestTaskMemory/Scripts/MainMenuController.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Misuno.TestTaskMemory
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        public void PlayButtonPressed()
+        {
+            SceneManager.LoadScene("Game");
+        }        
+    }
 }

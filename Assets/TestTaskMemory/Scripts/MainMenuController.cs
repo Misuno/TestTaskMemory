d7712cfa2ce6fecbdb65cdf36f211ca3f1d78025
Alex Misuno <alex@misuno.org>
2017-05-22 18:38:43 +0200

    Main menu

--- Assets/TestTaskMemory/Scripts/MainMenuController.cs
+++ Assets/TestTaskMemory/Scripts/MainMenuController.cs
@@ -7,9 +7,17 @@ namespace Misuno.TestTaskMemory
 {
     public class MainMenuController : MonoBehaviour
     {
+        [Range(0f, 600f)]
+        public float timerValue = 120f;
+
         public void PlayButtonPressed()
         {
             SceneManager.LoadScene("Game");
-        }        
+        }
+
+        public void ToggleTimer(bool toggled)
+        {
+            World.timer = toggled ? timerValue : 0f;
+        }
     }
 }
--- Assets/TestTaskMemory/Scripts/World.cs
+++ Assets/TestTaskMemory/Scripts/World.cs
@@ -8,5 +8,6 @@ namespace Misuno.TestTaskMemory
     public static class World
     {
         public static GameBoardController board;
+        public static float timer = 0f;
     }
 }

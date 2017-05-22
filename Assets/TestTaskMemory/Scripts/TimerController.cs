a129c1fb1ad1e65091f03c9c9dc2de22dc11d4ac
Alex Misuno <alex@misuno.org>
2017-05-22 18:58:55 +0200

    done

---
+++ Assets/TestTaskMemory/Scripts/TimerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Misuno.TestTaskMemory
+{
+    public class TimerController : MonoBehaviour
+    {
+        private float passedTime;
+        private float startTime;
+        private Image myImage;
+
+
+        void Awake()
+        {
+            myImage = GetComponent<Image>();
+        }
+
+        void Start()
+        {
+            if (World.timer > 0f)
+            {
+                startTime = Time.time;
+                myImage.fillAmount = 1f;
+                StartCoroutine(Timer());
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private IEnumerator Timer()
+        {
+            while (true)
+            {
+                float passedTime = Time.time - startTime;
+                if (passedTime >= World.timer)
+                {
+                    SceneManager.LoadScene("Menu");
+                    yield break;
+                }
+
+                float amount = 1f - passedTime / World.timer;
+                myImage.fillAmount = amount;
+
+                yield return null;
+            }
+        }
+    }
+}
